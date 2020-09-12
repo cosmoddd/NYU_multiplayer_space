12a180f50125cc8a25d9e84eef3cdb8fbbb60ee9
Luke Lin <lukelin2009@hotmail.com>
2020-09-12 15:16:20 -0700

    + MovementController with basic movement controls implemented + CameraController as a prototype phase third-person camera

---
+++ Assets/_Collaborators/Luke/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public float distFromTarget = 5.0f;
+    public float mouseSensitivity = 10.0f;
+    public Vector2 pitchMinMax = new Vector2(-45, 90);
+
+    float pitch, yaw;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        // yaw for looking side to side, pitch for looking up and down
+        yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
+        pitch -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+        pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
+
+        transform.eulerAngles = new Vector3(pitch, yaw);
+
+        transform.position = target.position - transform.forward * distFromTarget;
+    }
+}
