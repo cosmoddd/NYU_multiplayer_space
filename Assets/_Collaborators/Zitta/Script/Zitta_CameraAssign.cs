dcaf62510c9f6c9e31090ed19c080fa8ae4f0b0d
zittacheng <zittacheng@gmail.com>
2020-10-12 12:05:10 -0400

    Toys (without camera mod and interaction system)

---
+++ Assets/_Collaborators/Zitta/Script/Zitta_CameraAssign.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zitta_CameraAssign : MonoBehaviour {
+    public bool Assigned;
+
+    public void Awake()
+    {
+        if (Camera.main && !Assigned)
+        {
+            Assign();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Camera.main && !Assigned)
+        {
+            Assign();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Camera.main && !Assigned)
+        {
+            Assign();
+        }
+    }
+
+    public void Assign()
+    {
+        gameObject.transform.parent = Camera.main.transform;
+        gameObject.transform.localPosition = new Vector3();
+        Assigned = true;
+        Destroy(this);
+    }
+}
---
+++ Assets/_Collaborators/Zitta/Script/Zitta_SpookySound.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zitta_SpookySound : MonoBehaviour {
+    public static Zitta_SpookySound Main;
+    public AudioSource Source;
+    public float VolumeSpeed;
+    public bool Active;
+
+    public void Awake()
+    {
+        Main = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Active && Source.volume < 1)
+            Source.volume += VolumeSpeed * Time.deltaTime;
+        else if (!Active && Source.volume > 0)
+            Source.volume -= VolumeSpeed * Time.deltaTime;
+        if (Source.volume > 1)
+            Source.volume = 1;
+        else if (Source.volume < 0)
+            Source.volume = 0;
+    }
+
+    public void Play()
+    {
+        Active = true;
+    }
+
+    public void Stop()
+    {
+        Active = false;
+    }
+}
