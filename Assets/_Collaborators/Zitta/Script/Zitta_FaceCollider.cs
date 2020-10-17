5b9bdcd3844c4c704e081273188125937d5f2498
zittacheng <zittacheng@gmail.com>
2020-10-17 06:34:52 -0400

    Block

---
+++ Assets/_Collaborators/Zitta/Script/Zitta_FaceCollider.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class Zitta_FaceCollider : MonoBehaviour {
+    public Zitta_Block Block;
+    public int Index;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Interact()
+    {
+        Block.CmdChangeValue(Index);
+    }
+}
