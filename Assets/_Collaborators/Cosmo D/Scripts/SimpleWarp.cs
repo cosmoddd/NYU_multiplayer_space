373e72d3fe06615965badfeb3390a25f4f2cf9e1
Cosmo D <gregheff@gmail.com>
2020-11-17 16:48:33 -0500

    prep for playtest

---
+++ Assets/_Collaborators/Cosmo D/Scripts/SimpleWarp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class SimpleWarp : NetworkBehaviour
+{
+
+    public GameObject warpLocation;
+
+    void OnTriggerEnter(Collider other)
+    {
+      if (isClient)
+      {
+        print(other.gameObject.name + "is trying to rise!");
+        other.GetComponent<CharacterController>().enabled = false;
+        other.transform.position = warpLocation.transform.position;
+        other.GetComponent<CharacterController>().enabled = true;
+      }
+    }
+
+}
