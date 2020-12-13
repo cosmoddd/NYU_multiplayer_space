6e7dee19e29238160ad21157961df9f6d0315c9c
MHOSS <hhoossppee@gmail.com>
2020-12-13 18:52:43 -0500

    Staged Charles Huang

---
+++ Assets/_WINTER SHOWCASE ASSETS/16) Charles Huang/RotateOverTime.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateOverTime : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        // Slowly rotate the object arond its X axis at 1 degree/second.
+        transform.Rotate(0, (Time.deltaTime * 10), 0);
+
+        // ... at the same time as spinning it relative to the global
+        // Y axis at the same speed.
+        //transform.Rotate(0, Time.deltaTime, 0, Space.World);
+    }
+}
