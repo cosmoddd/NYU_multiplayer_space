c900d1b64d238d7b4f02d205e4196d9f76891e36
lin <zl1836@nyu.edu>
2020-12-16 02:37:06 +0800

    Staged the work of Miles Rosenthal

---
+++ Assets/_WINTER SHOWCASE ASSETS/Miles-Rosenthal/rotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotation : MonoBehaviour
+{
+Transform tf;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tf = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        tf.transform.Rotate(0,1,0);
+    }
+}
