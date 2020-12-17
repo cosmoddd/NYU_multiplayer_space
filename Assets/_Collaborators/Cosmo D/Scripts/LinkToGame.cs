42d0f8438b5fec16e1bf0249fb0d47164f1462bf
Cosmo D <gregheff@gmail.com>
2020-12-16 19:37:21 -0500

    mid-staging w new labels

---
+++ Assets/_Collaborators/Cosmo D/Scripts/LinkToGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkToGame : MonoBehaviour
+{
+  public string thisURL;
+
+    public void OpenUrl()
+    {
+      Application.OpenURL(thisURL);
+    }
+}
