2ef017dc2a3696ee3131d9ce0370424cdbbcf4c6
Cosmo D <gregheff@gmail.com>
2021-02-03 23:01:27 -0500

    Snuck in a quit button in the login menu.

---
+++ Assets/_Collaborators/Cosmo D/Scripts/SimpleQuit.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleQuit : MonoBehaviour
+{
+    public void SimpleQuitFunc()
+    {
+        Application.Quit();
+    }
+}
